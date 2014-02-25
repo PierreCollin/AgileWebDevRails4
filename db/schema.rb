cd19e40a08d2bc1afcb1b92867dc71d32f6b12e0
Pierre Collin <pierre@pcollin.com>
2014-02-25 12:27:57 +0100

    Iteration D1

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140217162354) do
+ActiveRecord::Schema.define(version: 20140225101525) do
+
+  create_table "carts", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", force: true do |t|
     t.string   "title"
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -15,8 +15,7 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
-    assert_select '#columns #side a', minimum: 4
-    
+    assert_select '#columns #side a', minimum: 4w
     assert_select 'h1', 'Listing products'
   end
 
