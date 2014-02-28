a28015f709569194bdb1fbfce10f3e350e7065b1
Pierre Collin <pierre@pcollin.com>
2014-02-28 12:16:55 +0100

    Iteration E2

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -5,6 +5,7 @@ class LineItemsController < ApplicationController
   before_action :set_cart, only: [:create]
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
 
+
   # GET /line_items
   # GET /line_items.json
   def index
@@ -74,6 +75,6 @@ class LineItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
+      params.require(:line_item).permit(:product_id)
     end
 end
