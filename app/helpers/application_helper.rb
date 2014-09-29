8f4b94d1fb7b6746621382b13650b00a634a00cf
Pierre Collin <pierre@pcolllin.com>
2014-09-29 22:56:43 +0200

    Iteration F4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,13 @@
 module ApplicationHelper
 
-	def formatted_price price
-		number_to_currency price, separator: '.', unit: '€', format: '%n %u'
-	end
+  def formatted_price price
+    number_to_currency price, separator: '.', unit: '€', format: '%n %u'
+  end
 
+  def hidden_div_if(condition, attributes={}, &block)
+    if condition
+      attributes['style'] = 'display: none'
+    end
+    content_tag(:div, attributes, &block)
+  end
 end
